95563bc2d2418b3ea91afd671406ee078b0a7fa9
Attila Mihaly <attila.mihaly.jr@gmail.com>
2017-04-03 20:27:25 -0400

    Experiment

--- build.sbt
+++ build.sbt
@@ -6,7 +6,11 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
+
 libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.1"
+
+libraryDependencies += "com.thoughtworks.binding" %%% "dom" % "10.0.2"
 
 libraryDependencies += "com.lihaoyi" %%% "utest" % "0.4.4" % "test"
 testFrameworks += new TestFramework("utest.runner.Framework")
@@ -18,3 +22,5 @@ jsDependencies += "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
 jsDependencies += RuntimeDOM
 
 persistLauncher := true
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,47 +1,67 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
-import org.scalajs.jquery.{ JQuery, jQuery }
-import tutorial.webapp.Node.{ Elem, Text }
+import scala.scalajs.js.annotation.JSExport
+import com.thoughtworks.binding.Binding.{ BindingSeq, Var, Vars }
+import com.thoughtworks.binding.{ Binding, dom }
+import org.scalajs.dom.{ Event, Node, document }
 
 object TutorialApp extends JSApp {
 
-    def main(): Unit = {
-        jQuery(setupUI _)
-    }
+    implicit def makeIntellijHappy(x: scala.xml.Node): Binding[org.scalajs.dom.raw.Node] = ???
+    implicit def makeIntellijHappy(x: scala.xml.NodeBuffer): Binding[BindingSeq[org.scalajs.dom.raw.Node]] = ???
 
-    def setupUI(): Unit = {
-//        jQuery("""<button type="button">Click me!</button>""")
-//            .click(addClickedMessage _)
-//            .appendTo(jQuery("body"))
-//        jQuery("body").append("<p>Hello World</p>")
-        renderNode (
-            document.body,
-            document.body.firstElementChild,
-            None,
-            Some (Elem ("b", Seq (Text ("hi"))))
-        )
-    }
+    case class Contact(name: Var[String], email: Var[String])
 
-    def addClickedMessage(): Unit = {
-        jQuery("body").append("<p>You clicked the button!</p>")
-    }
+    val data = Vars.empty[Contact]
 
-    def renderNode (parent : dom.Node, current : dom.Node, from : Option [Node], to : Option [Node]) : Unit = {
-        (from, to) match {
-            case (Some (Elem (fromName, fromChildren)), Some (Elem (toName, toChildren))) if fromName == toName =>
-                ???
-            case (_, Some (Elem (name, children))) =>
-                val elemNode = document.createElement (name)
-                for (child <- children) {
-                    renderNode (current, elemNode, None, Some (child))
+    @dom
+    def table: Binding[BindingSeq[Node]] = {
+        <div>
+            <button
+            onclick={ event: Event =>
+                data.get += Contact(Var("Yang Bo"), Var("yang.bo@rea-group.com"))
+            }
+            >
+                Add a contact
+            </button>
+        </div>
+        <table border="1" cellPadding="5">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                for (contact <- data) yield {
+                    <tr>
+                        <td>
+                            {contact.name.bind}
+                        </td>
+                        <td>
+                            {contact.email.bind}
+                        </td>
+                        <td>
+                            <button
+                            onclick={ event: Event =>
+                                contact.name := "Modified Name"
+                            }
+                            >
+                                Modify the name
+                            </button>
+                        </td>
+                    </tr>
                 }
-                parent.replaceChild (elemNode, current)
-            case (_, Some (Text (text))) =>
-                current.appendChild (document.createTextNode (text))
-        }
+                }
+            </tbody>
+        </table>
+    }
+
+    @JSExport
+    def main() : Unit = {
+        dom.render (document.body, table)
     }
 
 }
